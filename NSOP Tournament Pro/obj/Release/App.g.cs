c6b3b14eeb1aa6b1000eb9afef138aa7536cde71
ove ronny hauge <ovehauge@hotmail.no>
2020-01-24 00:24:34 +0100

    dd

--- NSOP Tournament Pro/obj/Release/App.g.cs
+++ NSOP Tournament Pro/obj/Release/App.g.cs
@@ -39,7 +39,7 @@ namespace NSOP_Tournament_Pro {
     /// <summary>
     /// App
     /// </summary>
-    public partial class App : System.Windows.Application {
+    public partial class App : System.Windows.Application, System.Windows.Markup.IComponentConnector {
         
         private bool _contentLoaded;
         
@@ -49,16 +49,16 @@ namespace NSOP_Tournament_Pro {
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.0.0.0")]
         public void InitializeComponent() {
-            if (_contentLoaded) {
-                return;
-            }
-            _contentLoaded = true;
             
             #line 5 "..\..\App.xaml"
             this.StartupUri = new System.Uri("MainWindow.xaml", System.UriKind.Relative);
             
             #line default
             #line hidden
+            if (_contentLoaded) {
+                return;
+            }
+            _contentLoaded = true;
             System.Uri resourceLocater = new System.Uri("/NSOP Tournament Pro;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\App.xaml"
@@ -68,16 +68,14 @@ namespace NSOP_Tournament_Pro {
             #line hidden
         }
         
-        /// <summary>
-        /// Application Entry Point.
-        /// </summary>
-        [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.0.0.0")]
-        public static void Main() {
-            NSOP_Tournament_Pro.App app = new NSOP_Tournament_Pro.App();
-            app.InitializeComponent();
-            app.Run();
+        [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
+        [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1800:DoNotCastUnnecessarily")]
+        void System.Windows.Markup.IComponentConnector.Connect(int connectionId, object target) {
+            this._contentLoaded = true;
         }
     }
 }
